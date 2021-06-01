1e3c9f509baeb1df0e356db695411fdd420c6aa7
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-06-01 16:45:34 +0200

    filter - half working

--- shop_script.js
+++ shop_script.js
@@ -1,18 +1,43 @@
-const URL =
-  'https://kea21s-6eb0.restdb.io/rest/leve-products?q={"$and" : [{"$or": [{"category": "blazers"}, {"category": "pants"}]}, {"$or": [{"color": "mint"}, {"color": "navy blue"}]} ]}';
-fetch(URL, {
-  method: "GET",
-  headers: {
-    "x-apikey": "606d606af55350043100752e",
-  },
-})
-  .then((res) => res.json())
-  .then((data) => {
-    listProducts(data);
+const URL = "https://kea21s-6eb0.restdb.io/rest/leve-products";
+
+load();
+
+function filter(filter_para) {
+  console.log("filtering", filter_para);
+  document.querySelector(".list").innerHTML = "";
+  fetch(
+    `https://kea21s-6eb0.restdb.io/rest/leve-products?q={ "$or": ${filter_para} }`,
+    {
+      method: "GET",
+      headers: {
+        "x-apikey": "606d606af55350043100752e",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      listProducts(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+function load() {
+  fetch(URL, {
+    method: "GET",
+    headers: {
+      "x-apikey": "606d606af55350043100752e",
+    },
   })
-  .catch((err) => {
-    console.error(err);
-  });
+    .then((res) => res.json())
+    .then((data) => {
+      listProducts(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
 
 function listProducts(data) {
   data.forEach(showProduct);
@@ -65,4 +90,106 @@ function dropDown4() {
   console.log("function dropDown4()");
   document.querySelector("#filter4").textContent = "+";
   document.querySelector(".size").classList.toggle("disappear");
+}
+
+const collCheckbox = document
+  .querySelector("#collection")
+  .querySelectorAll(`[type="checkbox"]`);
+collCheckbox.forEach((e) => {
+  e.onclick = () => {
+    filterCollections();
+  };
+});
+
+function filterCollections() {
+  let array = [];
+
+  collCheckbox.forEach((e) => {
+    if (e.checked === true) {
+      array.push({
+        collection: `${e.dataset.category}`,
+      });
+    }
+  });
+
+  filter(JSON.stringify(array));
+}
+
+const catCheckbox = document
+  .querySelector("#category")
+  .querySelectorAll(`[type="checkbox"]`);
+catCheckbox.forEach((e) => {
+  e.onclick = () => {
+    filterCategories();
+  };
+});
+
+function filterCategories() {
+  let array = [];
+
+  catCheckbox.forEach((e) => {
+    if (e.checked === true) {
+      array.push({
+        category: `${e.dataset.category}`,
+      });
+    }
+  });
+
+  filter(JSON.stringify(array));
+}
+
+const low = document.querySelector("#LowToHigh");
+const high = document.querySelector("#HighToLow");
+low.addEventListener("click", sortLow);
+high.addEventListener("click", sortHigh);
+
+function sortLow() {
+  console.log("function sortLow()");
+  document.querySelector(".list").innerHTML = "";
+  fetch(
+    `https://kea21s-6eb0.restdb.io/rest/leve-products?q={}&sort=price&sort=sale_price&dir=1`,
+    {
+      method: "GET",
+      headers: {
+        "x-apikey": "606d606af55350043100752e",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      listProducts(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  low.addEventListener("click", reload);
+}
+
+function sortHigh() {
+  console.log("function sortLow()");
+  document.querySelector(".list").innerHTML = "";
+  fetch(
+    `https://kea21s-6eb0.restdb.io/rest/leve-products?q={}&sort=price&sort=sale_price&dir=-1`,
+    {
+      method: "GET",
+      headers: {
+        "x-apikey": "606d606af55350043100752e",
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      listProducts(data);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  high.addEventListener("click", reload);
+}
+
+function reload() {
+  document.querySelector(".list").innerHTML = "";
+  load();
 }
