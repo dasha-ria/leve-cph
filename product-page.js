be388dcfc1381d48727b76221e030f1021b470bf
Daria Stoica <dariastoicad@gmail.com>
2021-06-05 16:55:28 +0200

    now you can choose pics

--- product-page.js
+++ product-page.js
@@ -43,47 +43,46 @@ function frontPic() {
 
 frontPic();
 
-// function frontPic-mobile() {
-//     document.querySelector("#mobile-pic-1").classList.remove("product-pic-hidden");
-//     document.querySelector("#mobile-pic-2").classList.add("product-pic-hidden");
-//     document.querySelector("#mobile-pic-3").classList.add("product-pic-hidden");
-//     document.querySelector("#mobile-pic-4").classList.add("product-pic-hidden");
+function clickPicmobile() {
+  let mobilepic1 = document.querySelector("#mobile-pic-1");
+  let mobilepic2 = document.querySelector("#mobile-pic-2");
+  let mobilepic3 = document.querySelector("#mobile-pic-3");
+  let mobilepic4 = document.querySelector("#mobile-pic-4");
 
-//     const showpic1 = document.querySelector("#click-pic-1");
+  let pictures = [mobilepic1, mobilepic2, mobilepic3, mobilepic4];
 
-//     showpic1.onclick = () => {
-//       document.querySelector("#mobile-pic-1").classList.remove("product-pic-hidden");
-//       document.querySelector("#mobile-pic-2").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-3").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-4").classList.add("product-pic-hidden");
-//     };
+  let nextButton = document.querySelector(".product-arrow-right");
+  let previousButton = document.querySelector(".product-arrow-left");
 
-//     const showpic2 = document.querySelector("#click-pic-2");
+  nextButton.onclick = () => {
+    for (let i = 0; i < pictures.length; i++) {
+      if (!pictures[i].classList.contains("product-pic-hidden")) {
+        pictures[i].classList.add("product-pic-hidden");
 
-//     showpic2.onclick = () => {
-//       document.querySelector("#mobile-pic-1").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-2").classList.remove("product-pic-hidden");
-//       document.querySelector("#mobile-pic-3").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-4").classList.add("product-pic-hidden");
-//     };
+        if (i + 1 === pictures.length) {
+          pictures[0].classList.remove("product-pic-hidden");
+        } else {
+          pictures[i + 1].classList.remove("product-pic-hidden");
+        }
+        break;
+      }
+    }
+  };
 
-//     const showpic3 = document.querySelector("#click-pic-3");
+  previousButton.onclick = () => {
+    for (let i = 0; i < pictures.length; i++) {
+      if (!pictures[i].classList.contains("product-pic-hidden")) {
+        pictures[i].classList.add("product-pic-hidden");
 
-//     showpic3.onclick = () => {
-//       document.querySelector("#mobile-pic-1").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-2").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-3").classList.remove("product-pic-hidden");
-//       document.querySelector("#mobile-pic-4").classList.add("product-pic-hidden");
-//     };
+        if (i === 0) {
+          pictures[pictures.length - 1].classList.remove("product-pic-hidden");
+        } else {
+          pictures[i - 1].classList.remove("product-pic-hidden");
+        }
+        break;
+      }
+    }
+  };
+}
 
-//     const showpic4 = document.querySelector("#click-pic-4");
-
-//     showpic4.onclick = () => {
-//       document.querySelector("#mobile-pic-1").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-2").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-3").classList.add("product-pic-hidden");
-//       document.querySelector("#mobile-pic-4").classList.remove("product-pic-hidden");
-//     };
-//   }
-
-//   frontPic-mobile();
+clickPicmobile();
