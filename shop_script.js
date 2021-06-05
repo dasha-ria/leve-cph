f366f307aa4f1324ec0c17bddfd84f2281343dbf
Daria Stoica <dariastoicad@gmail.com>
2021-06-05 20:50:16 +0200

    worked on shop script, to stop the event listener from adding products

--- shop_script.js
+++ shop_script.js
@@ -1,5 +1,9 @@
 const URL = "https://kea21s-6eb0.restdb.io/rest/leve-products";
 
+const state = {
+  sortBy: null,
+};
+
 load();
 
 function filter(filter_para) {
@@ -140,8 +144,18 @@ function filterCategories() {
 
 const low = document.querySelector("#LowToHigh");
 const high = document.querySelector("#HighToLow");
-low.addEventListener("click", sortLow);
-high.addEventListener("click", sortHigh);
+low.addEventListener("click", handleSortByLow);
+high.addEventListener("click", handleSortByHigh);
+
+function handleSortByLow() {
+  if (state.sortBy === "low") {
+    reload();
+    state.sortBy = null;
+  } else {
+    sortLow();
+    state.sortBy = "low";
+  }
+}
 
 function sortLow() {
   console.log("function sortLow()");
@@ -163,7 +177,17 @@ function sortLow() {
       console.error(err);
     });
 
-  low.addEventListener("click", reload);
+  // low.addEventListener("click", reload);
+}
+
+function handleSortByHigh() {
+  if (state.sortBy === "high") {
+    reload();
+    state.sortBy = null;
+  } else {
+    sortHigh();
+    state.sortBy = "high";
+  }
 }
 
 function sortHigh() {
@@ -186,7 +210,7 @@ function sortHigh() {
       console.error(err);
     });
 
-  high.addEventListener("click", reload);
+  // high.addEventListener("click", reload);
 }
 
 function reload() {
